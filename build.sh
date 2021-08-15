71bff03ebba171ba1f709d8ef4a91882346f831c
Ubuntu <ubuntu@ip-172-31-13-161.us-east-2.compute.internal>
2021-08-15 03:42:03 +0000

    added push to harbor

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@ VERSION="`cat Version`"
 
 docker build --no-cache -t harbor.tanzu.bekind.io/demoapps/tanzu-shell:latest -t harbor.tanzu.bekind.io/demoapps/tanzu-shell:${VERSION} .
 
+docker push harbor.tanzu.bekind.io/demoapps/tanzu-shell:${VERSION} 
+docker push harbor.tanzu.bekind.io/demoapps/tanzu-shell:latest
